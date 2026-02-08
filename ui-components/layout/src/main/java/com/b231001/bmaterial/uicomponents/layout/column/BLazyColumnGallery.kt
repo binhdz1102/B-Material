3e6ee329464bf7198954b7aecb101c73f6d90085
Binhdz1102 <husemail00@gmail.com>
2026-02-08 14:56:17 +0700

    refactor

--- ui-components/layout/src/main/java/com/b231001/bmaterial/uicomponents/layout/column/BLazyColumnGallery.kt
+++ ui-components/layout/src/main/java/com/b231001/bmaterial/uicomponents/layout/column/BLazyColumnGallery.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -27,7 +28,7 @@ fun BLazyColumnGallery() {
     val bState = rememberBLazyListState()
     val scope = rememberCoroutineScope()
 
-    Column(Modifier.fillMaxSize()) {
+    Column(Modifier.height(500.dp)) {
         Row(
             Modifier
                 .fillMaxWidth()
